9d0a2776f2ecdce86bd6bee563e73a87c759cd36
rizex1704 <rizex1704@gmail.com>
2024-03-25 20:03:27 +0900

    secondCommit

--- baekjoon/1001/a.c
+++ baekjoon/1001/a.c
@@ -2,7 +2,13 @@
 
 int main(void)
 {
-    char c='A';
-    printf("%d, %f, %c",c, c, c);
+    printf("int 자료형의 크기:%ld바이트\n", sizeof(int));
+    printf("float 자료형의 크기:%ld바이트\n", sizeof(float));
+    printf("double 자료형의 크기:%ld바이트\n",sizeof(double));
+    printf("char 자료형의 크기:%ld바이트\n", sizeof(char));
+    printf("long 자료형의 크기:%ld바이트\n",sizeof(long));
+    printf("long long int 자료형의 크기:%ld바이트\n",sizeof(long long int));
+    printf("unsigned 자료형의 크기:%ld바이트",sizeof(unsigned int));
     return 0;
+
 }
