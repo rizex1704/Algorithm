115f9c6ae004ab8b4ee698d85227383385c80d89
rizex1704 <rizex1704@gmail.com>
2024-04-08 19:07:28 +0900

    0408

--- 내용/pointer/ptrfnc.c
+++ 내용/pointer/ptrfnc.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-int *return_arr(){
-    int arr[5]={1,2,3,4,5};
-    return(arr);
+int sum(int a,int b){
+    
+    return(a+b);
 }
 
 int main(){
-    printf("%d\n",*return_arr());
+    printf("%d\n",sum(1,2));
 }
